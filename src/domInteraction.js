e841937ffc97d9d0a7fe88952333a20dde5482df
Grelgn <grelgn@hotmail.com>
2023-12-04 21:34:53 +0300

    Implement clicking enemy board and hit detection

--- src/domInteraction.js
+++ src/domInteraction.js
@@ -23,6 +23,28 @@ function populateUser() {
 	}
 }
 
+function clickEnemy() {
+	const aiBoardY = document.querySelectorAll(".enemy-board > div");
+	aiBoardY.forEach((Y) => {
+		const aiBoardX = Y.querySelectorAll("div");
+		aiBoardX.forEach((X) => {
+			X.addEventListener("click", () => {
+				const y = X.id.slice(3, 4);
+				const x = X.id.slice(5);
+				console.log(X.id);
+				console.log(y);
+				console.log(x);
+				if (AI.gameBoard.array[y][x] !== " ") {
+					X.classList.add("hit");
+				} else {
+                    X.classList.add("miss");
+				}
+			});
+		});
+	});
+}
+
 export default function domInteraction() {
 	populateUser();
+	clickEnemy();
 }
